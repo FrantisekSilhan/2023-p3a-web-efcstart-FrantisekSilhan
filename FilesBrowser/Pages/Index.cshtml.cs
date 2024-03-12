57e969805a49eb3bb8e72a3021d140e7f99c566b
František Šilhán <frantisek@slhn.cz>
2024-03-12 10:35:18 +0100

    funguje

--- FilesBrowser/Pages/Index.cshtml.cs
+++ FilesBrowser/Pages/Index.cshtml.cs
@@ -12,6 +12,9 @@ namespace FilesBrowser.Pages
         private readonly DatabaseContext _context;
 
         public Folder Folder { get; set; }
+        public List<Folder> Subfolders { get; set; }
+        public Guid FolderId { get; set; } = Guid.Empty;
+        public Guid ParentFolderId { get; set; } = Guid.Empty;
 
         public IndexModel(ILogger<IndexModel> logger, DatabaseContext context)
         {
@@ -28,6 +31,20 @@ namespace FilesBrowser.Pages
                 Folder = _context.Folders.FirstOrDefault(f => f.FolderId == folder);
             }
             Folder.Folders = _context.Folders.Where(f => f.ParentFolderId == Folder.FolderId).ToList();
+            ParentFolderId = folder;
+        }
+
+        public void OnGetOpen(Guid folder, Guid subfolder) {
+            if (folder == Guid.Empty) {
+                Folder = _context.Folders.FirstOrDefault(f => f.Name == "Root");
+            } else {
+                Folder = _context.Folders.FirstOrDefault(f => f.FolderId == folder);
+            }
+            Folder.Folders = _context.Folders.Where(f => f.ParentFolderId == Folder.FolderId).ToList();
+
+            ParentFolderId = folder;
+            FolderId = subfolder;
+            Subfolders = _context.Folders.Where(s => s.ParentFolderId == FolderId).ToList();
         }
     }
 }
