21bd1f9e2ac5b3e2d1b3abb6fb18e615f930588b
František Šilhán <frantisek@slhn.cz>
2024-03-12 09:30:23 +0100

    třetinový commit

--- FilesBrowser/Data/DatabaseContext.cs
+++ FilesBrowser/Data/DatabaseContext.cs
@@ -6,6 +6,46 @@ namespace FilesBrowser.Data {
         public DbSet<Folder> Folders { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Folder>()
+                .HasMany(f => f.Folders)
+                .WithOne(f => f.ParentFolder)
+                .HasForeignKey(f => f.ParentFolderId);
+
+            Folder rootFolder = new Folder { FolderId = Guid.NewGuid(), Name = "Root" };
+
+            modelBuilder.Entity<Folder>().HasData(
+                new Folder { FolderId = Guid.NewGuid(), Name = "Music", ParentFolderId = rootFolderId },
+                new Folder { FolderId = Guid.NewGuid(), Name = "Images", ParentFolderId = rootFolderId },
+                new Folder { FolderId = Guid.NewGuid(), Name = "Orchestral", ParentFolderId = musicFolderId },
+                new Folder { FolderId = Guid.NewGuid(), Name = "Pop", ParentFolderId = musicFolderId },
+                new Folder { FolderId = Guid.NewGuid(), Name = "Rock", ParentFolderId = musicFolderId },
+                new Folder { FolderId = Guid.NewGuid(), Name = "Family", ParentFolderId = imagesFolderId },
+                new Folder { FolderId = Guid.NewGuid(), Name = "Wallpapers", ParentFolderId = imagesFolderId },
+                new Folder { FolderId = Guid.NewGuid(), Name = "Screenshots", ParentFolderId = imagesFolderId }
+            );
+
+            Folder musicFolder = new Folder { FolderId = Guid.NewGuid(), Name = "Music", Folders = new List<Folder>
+                {
+                    new Folder { FolderId = Guid.NewGuid(), Name = "Orchestral" },
+                    new Folder { FolderId = Guid.NewGuid(), Name = "Pop" },
+                    new Folder { FolderId = Guid.NewGuid(), Name = "Rock" },
+
+                }
+            };
+            Folder imagesFolder = new Folder { FolderId = Guid.NewGuid(), Name = "Images", Folders = new List<Folder>
+                {
+                    new Folder { FolderId = Guid.NewGuid(), Name = "Family" },
+                    new Folder { FolderId = Guid.NewGuid(), Name = "Wallpapers" },
+                    new Folder { FolderId = Guid.NewGuid(), Name = "Screenshots" },
+                }
+            };
+
+            modelBuilder.Entity<Folder>(g => {
+                g.HasData(
+                    new Folder { FolderId = Guid.NewGuid(), Name = "Root", Folders = new List<Folder> { musicFolder, imagesFolder } }
+                );
+            });
         }
     }
 }
--- FilesBrowser/Models/Folder.cs
+++ FilesBrowser/Models/Folder.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FilesBrowser.Models {
     public class Folder {
         public Guid FolderId { get; set; }
-        public string Name { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
         public ICollection<Folder>? Folders { get; set; }
         [ForeignKey(nameof(ParentFolderId))]
         public Guid ParentFolderId { get; set; }
